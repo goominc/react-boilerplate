ae706b08bc7ae3b9ab10408f41d4ff1a49c8b325
Hyoungwook Kim <hukim99@gmail.com>
2016-08-24 12:57:45 +0900

    Bundles for both desktop & mobile.

--- webpack.config.js
+++ webpack.config.js
@@ -4,65 +4,66 @@ const path = require('path');
 const webpack = require('webpack');
 
 const mode = process.env.NODE_ENV || 'development';
-const device = process.env.DEVICE || 'desktop';
+const devices = ['desktop', 'mobile'];
 
-const configuration = {};
-const entry = [`./${device}/main`];
-const plugins = [
-  new webpack.DefinePlugin({
-    'process.env': {
-      NODE_ENV: JSON.stringify(mode),
+module.exports = devices.map((device) => {
+  const entry = [`./${device}/main`];
+  const plugins = [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify(mode),
+      },
+    }),
+    new webpack.optimize.OccurenceOrderPlugin(),
+  ];
+  const configuration = {
+    entry,
+    output: {
+      path: __dirname,
+      filename: `${device}.bundle.js`,
     },
-  }),
-  new webpack.optimize.OccurenceOrderPlugin(),
-];
-
-if (mode === 'production') {
-  plugins.push(new webpack.optimize.DedupePlugin());
-  plugins.push(new webpack.optimize.UglifyJsPlugin({
-    compress: {
-      warnings: false,
+    resolve: {
+      extensions: ['', '.js', '.jsx'],
+      root: [
+        path.resolve(__dirname),
+        path.resolve(__dirname, device),
+      ],
     },
-  }));
-} else {
-  configuration.devtool = 'source-map';
-
-  entry.push('webpack-hot-middleware/client');
-  plugins.push(new webpack.HotModuleReplacementPlugin());
-}
-
-module.exports = Object.assign(configuration, {
-  entry,
-  output: {
-    path: __dirname,
-    filename: 'bundle.js',
-  },
-  resolve: {
-    extensions: ['', '.js', '.jsx'],
-    root: [
-      path.resolve(__dirname),
-      path.resolve(__dirname, device),
-    ],
-  },
-  plugins,
-  module: {
-    loaders: [
-      {
-        test: /\.jsx?$/,
-        loader: 'babel',
-        exclude: /node_modules/,
-        query: {
-          presets: ['es2015', 'react'],
-          plugins: [['react-intl', {
-            messagesDir: './build/messages/',
-            enforceDescriptions: true,
-          }]],
+    plugins,
+    module: {
+      loaders: [
+        {
+          test: /\.jsx?$/,
+          loader: 'babel',
+          exclude: /node_modules/,
+          query: {
+            presets: ['es2015', 'react'],
+            plugins: [['react-intl', {
+              messagesDir: './build/messages/',
+              enforceDescriptions: true,
+            }]],
+          },
         },
+        {
+          test: /\.scss$/,
+          loaders: ['style', 'css', 'sass'],
+        },
+      ],
+    },
+  };
+
+  if (mode === 'production') {
+    plugins.push(new webpack.optimize.DedupePlugin());
+    plugins.push(new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false,
       },
-      {
-        test: /\.scss$/,
-        loaders: ['style', 'css', 'sass'],
-      },
-    ],
-  },
+    }));
+  } else {
+    configuration.devtool = 'source-map';
+
+    entry.push('webpack-hot-middleware/client');
+    plugins.push(new webpack.HotModuleReplacementPlugin());
+  }
+  return configuration;
 });
