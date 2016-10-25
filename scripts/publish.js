653a3e9afa4ea579df5a0bc1cfed221efdf1786a
Hyoungwook Kim <k@alina.ai>
2016-10-25 14:36:40 +0900

    Pug for index.html.

--- scripts/publish.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-
-['desktop', 'mobile'].forEach((device) => {
-  let html = fs.readFileSync(path.join(__dirname, `../${device}/index.html`)).toString();
-  html = html.replace('</head>', `  <link rel="stylesheet" type="text/css" href="/dist/${device}.css">\n  </head>`);
-  fs.writeFileSync(path.join(__dirname, `../dist/${device}.html`), html);
-});
